00ec2317efdfe015e98dac6633b865ca13815f36
shivji-creator <80440513+shivji-creator@users.noreply.github.com>
2021-03-12 08:11:36 +0530

    Add files via upload

---
+++ main (2).cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+ {
+int n;
+cout<<"Enter age:";
+cin>>n;
+if(n>=18)cout<<"Eligible to use vote.\n";
+else cout<<"Not eligible to use vote\n";
+return 0;
+}
